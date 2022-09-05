fd96217ebef7a21a69bceac4180f11b1d66bcebf
meguia <eguiamc@gmail.com>
2022-09-05 10:53:06 -0300

    cleaned teensyThunder

--- Biocenosis_teensyThunder/old/play.cmd
+++
@@ -1,6 +0,0 @@
-echo 'WIND 0.0 0.0' > /dev/ttyACM0
-echo 'GAINS 0.5 0.1 0.05 0.05' > /dev/ttyACM0
-echo 'DELAYS 10 500 600 700' > /dev/ttyACM0
-echo 'FILES 2 2 2 2' > /dev/ttyACM0
-echo 'FILTERS 5000 3000 1000 500' > /dev/ttyACM0
-echo 'PLAY' > /dev/ttyACM0
--- Biocenosis_teensyThunder/old/play.sh
+++
@@ -1,5 +0,0 @@
-echo 'FILTERS 5000.0 3000.0 1000.0 100.0' > /dev/ttyACM0
-echo 'LOCATION1 1 0 0 1' > /dev/ttyACM0
-echo 'LOCATION2 1 0 0 1' > /dev/ttyACM0
-echo 'FILE 2' > /dev/ttyACM0
-echo 'DELAYS 50 500 1000 1500' > /dev/ttyACM0
